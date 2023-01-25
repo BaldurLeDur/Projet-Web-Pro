21b530c9200dae54ba25362dcec720ea7e385b1a
unknown <sburton@gamingcampus.local>
2023-01-25 10:05:36 +0100

    Merge + Footer

---
+++ components/footer.php
@@ -0,0 +1,35 @@
+<footer class="page-footer">
+    <div class="container">
+        <div class="row">
+            <div class="col s5 footer-left">
+                <p>
+                    <a href="#">Nous contacter</a>
+                    <a href="#">Réservation</a>
+                    <a href="#">FAQ</a>
+                </p>
+            </div>
+            <div class="col s3">
+                <p>© THE SENSE, SAS. Tout droits réservés.</p>
+            </div>
+            <div class="col s3 footer-right">
+                <div class="text">
+                    <p>Modalités<span id="footer-separator"></span>Politique de confidentialité</p>
+                </div>
+                <div class="icons">
+                    <div class="icon">
+                        <i class="fa-brands fa-youtube"></i>
+                    </div>
+                    <div class="icon">
+                        <i class="fa-brands fa-instagram"></i>
+                    </div>
+                    <div class="icon">
+                        <i class="fa-brands fa-twitter"></i>
+                    </div>
+                    <div class="icon">
+                        <i class="fa-brands fa-facebook"></i>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</footer>
