8fae27e90de037bf8411b7f324b0ff3b6f9735f1
Sam-GTECH <115488661+Sam-GTECH@users.noreply.github.com>
2023-01-26 20:18:56 +0100

    Changed "require" to "require_once"

--- index.php
+++ index.php
@@ -7,8 +7,6 @@
   <!--Import materialize.css-->
   <link type="text/css" rel="stylesheet" href="css/materialize.css" media="screen,projection" />
   <link rel="stylesheet" href="css/style.css">
-  <link rel="stylesheet" href="css/style_article.css">
-
   <link href="css/fontawesome/fontawesome.min.css" rel="stylesheet">
   <link href="css/fontawesome/brands.min.css" rel="stylesheet">
   <link href="css/fontawesome/solid.min.css" rel="stylesheet">
@@ -72,20 +70,15 @@
   <?php require_once('components/article.php') ?>
   <?php require_once('components/about.php') ?>
 
-  <div class ="article_container">
-    <?php
-    require 'components/article.php';
-    ?>
-  </div>
-  <?php require 'components/faq.php'; ?>
-
-  <?php require("components/footer.php") ?>
+  <?php require_once("components/footer.php") ?>
 
 
   <script type="text/javascript" src="js/jquery.min.js"></script>
   <script type="text/javascript" src="js/materialize.min.js"></script>
   <script type="text/javascript" src="js/script.js"></script>
 </body>
+
+
 
 
 
