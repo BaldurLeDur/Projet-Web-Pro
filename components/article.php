de1387dc74a969ad91aa04c4e597d5bececa99b3
BaldurLeDur <102220784+BaldurLeDur@users.noreply.github.com>
2023-01-26 11:30:15 +0100

    saucisse

--- components/article.php
+++ components/article.php
@@ -1,7 +1,11 @@
-<div class="article">
-    <img src="asset/article_noel.svg">
-    <div class="texte">
-        <h2>ÉVÉNEMENT : LA LÉGENDE DU PÈRE NOËL</h2>
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellendus sed modi, vero dolore delectus voluptatum ullam nemo iste minus fuga molestias repellat et ab incidunt sequi expedita nam! Molestiae, iure.</p>
+<div class="container center"></div>
+    <div class="row">
+        <div class="col s12 imgarticle" ><img src="asset/article_noel.svg"></div>
+        <div class="col s1"></div>
+        <div class="back-text-article center col s10">
+            <h2>ÉVÉNEMENT : LA LÉGENDE DU PÈRE NOËL</h2>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellendus sed modi, vero dolore delectus voluptatum ullam nemo iste minus fuga molestias repellat et ab incidunt sequi expedita nam! Molestiae, iure.</p>
+        </div>
+        <div class="col s1"></div>
     </div>
 </div>
