2c1d49fbf561a21312993ba2df9c54c2c2405192
mlipko <mlipko@gaming.tech>
2023-01-27 11:59:30 +0100

    maj FAQ

--- components/faq.php
+++ components/faq.php
@@ -1,7 +1,7 @@
 <ul class="collapsible">
   <li>
     <div class="collapsible-header"><i class="material-icons">add_circle_outline</i>Qu'est ce que The SENSE ?</div>
-    <div class="collapsible-body"><span>Lorem ipsum dolor sit amet.</span></div>
+    <div class="collapsible-body"><span>The SENSE est une immersion poussée grâce à la réalité virtuelle. The SENSE propose de nombreuses expériences à faire entre amis ou avec la famille. Vous pouvez tout à fait favoriser une expérience avec de l'action comme notre Dark Room basé sur l'horreur. Ou encore, si vous le souhaiter par exemple, vous pouvez favoriser l'aspect compétitif en participant aux expériences de notre Battle Room. De plus, The SENSE propose un système de création d'experience où les clients peuvent à leurs tours imaginer et créer la meilleure expériences possibles.</span></div>
   </li>
   <li>
     <div class="collapsible-header"><i class="material-icons">add_circle_outline</i>Y a-t-il un âge et une taille minimum pour participer à une experience The SENSE</div>
