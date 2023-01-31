836234fa792452f34bd6f3b9b344b49db9b6ab69
Sam-GTECH <sburton@gaming.tech>
2023-01-31 13:26:58 +0100

    Finish connection module + send the navbar in components

--- components/navbar.php
+++ components/navbar.php
@@ -14,37 +14,49 @@
             <li class=" waves-effect waves-light"><a class="modal-trigger" href="#modal1"><span class="nav-component bold-nav">connexion</span></a></li>
         </ul>
     </div>
-</nav>
-<ul class="sidenav" id="mobile-demo">
-  <li class=" waves-effect waves-light"><a href="news.html"><span class="nav-component">News</span></a></li><br>
-  <li class=" waves-effect waves-light"><a href="exp.html"><span class="nav-component">Nos expérience</span></a></li><br>
-  <li class=" waves-effect waves-light"><a href="ebout.html"><span class="nav-component">à propos</span></a></li><br>
-  <li class=" waves-effect waves-light"><a class="modal-trigger" href="#modal1"><span class="nav-component">connexion</span></a></li>
-</ul> 
-<div id="modal1" class="modal">
-  <div class="modal-content">
-    <div class="row">
-      <form class="col s12">
-        <div class="row">
-          <div class="input-field col s12">
-            <input id="email" type="email" class="validate">
-            <label for="email">Email</label>
-          </div>
+  </nav>
+  <ul class="sidenav" id="mobile-demo">
+    <li class=" waves-effect waves-light"><a href="news.html"><span class="nav-component">News</span></a></li><br>
+    <li class=" waves-effect waves-light"><a href="exp.html"><span class="nav-component">Nos expérience</span></a></li><br>
+    <li class=" waves-effect waves-light"><a href="ebout.html"><span class="nav-component">à propos</span></a></li><br>
+    <li class=" waves-effect waves-light"><a class="modal-trigger" href="#modal1"><span class="nav-component">connexion</span></a></li>
+  </ul> 
+  <div id="modal1" class="modal">
+    <div class="modal-content">
+      <div class="row row-right">
+        <div class="col offset-l9 l1 offset-s3 modal-padding-bottom">
+          <p class="modal-upper-text">Connexion</p>
         </div>
-        <div class="row">
-          <div class="input-field col s12">
-            <input id="password" type="password" class="validate">
-            <label for="password">Password</label>
+        <form class="col s6">
+          <div class="row row-bottom">
+            <div class="col s12 l6">
+              <p>Identifiant</p>
+            </div>
+            <div class="input-field col s6 modal-email">
+              <input id="email" type="email">
+            </div>
           </div>
+          <div class="row row-bottom">
+            <div class="col s12 l6">
+              <p>Mot de passe</p>
+            </div>
+            <div class="input-field col s6 modal-password">
+              <input id="password" type="password">
+              <i onclick="PasswordReverse()" class="material-icons">remove_red_eye</i>
+              <a href="forgot_mdp.php"><p>Forgot your password?</p></a>
+            </div>
+          </div>
+        </form>
+      </div>
+    </div>
+    <div class="row" id="modal-foot">
+      <div class="width-diff left col offset-l2 l3 offset-s1 s12"><a href="sign_in.php">Créer un compte</a></div>
+
+
+      <a href="log_in.php">
+        <div class="col s3" id="sign">
+          Connexion
         </div>
-      </form>
+      </a>
     </div>
   </div>
-  <div class="row" id="modal-foot">
-    <div class="col s1"></div>
-    <a href="#!" class="modal-close" ><div class="col s2" id="connect">Connexion !</div></a>
-    <div class="left col s3"><a href="forgot_mdp.php">Mot de passe oublié ?</a></div>
-    <div class="col s3"><span class="right">Pas inscrit ?</span></div>
-    <div class="col s3" id="sign"><a href="sign_in.php">Inscrivez-vous !</a></div>
-  </div>
-</div>
