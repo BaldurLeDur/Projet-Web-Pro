df529544829ee77325c43887e3c7d749101ad13c
Fanat0x <117281041+Fanat0x@users.noreply.github.com>
2023-02-02 09:34:13 +0100

    experience

---
+++ components/carouselle.php
@@ -0,0 +1,20 @@
+<div class="carousel carousel-slider center">
+    <div class="carousel-fixed-item center">
+    </div>
+    <div class="carousel1" href="#one!">
+      <h2>First Panel</h2>
+      <p class="carousel1_text">This is your first panel</p>
+    </div>
+    <div class="carousel2" href="#two!">
+      <h2>Second Panel</h2>
+      <p class="carousel2_text">This is your second panel</p>
+    </div>
+    <div class="carousel3" href="#three!">
+      <h2>Third Panel</h2>
+      <p class="carousel3_text">This is your third panel</p>
+    </div>
+    <div class="carousel4" href="#four!">
+      <h2>Fourth Panel</h2>
+      <p class="carousel4_text">This is your fourth panel</p>
+    </div>
+  </div>
